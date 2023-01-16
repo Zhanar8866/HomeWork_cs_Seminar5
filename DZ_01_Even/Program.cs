f9ea7fdcbd3d082339ab1784d59b8dbf86588636
Zhanar S <zhan-ct@yandex.ru>
2023-01-16 17:56:46 +0300

    добавлено решение по Задаче 34 - количество чётных чисел

---
+++ DZ_01_Even/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] CreateRandomArray (int N, int start, int end)
+{
+    int[] RandomArray = new int [N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+void ShowArray (int[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write(Array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void NumberOfEvenNumbers (int[] Array)
+{
+    int sum = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] % 2 == 0)
+        {
+            sum+=1;
+        }
+     }
+     Console.WriteLine($"Количество чётных чисел в массиве равно {sum}");
+}
+
+Console.Write("Введите количество элементов массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] Array = CreateRandomArray (num, 100, 999);
+ShowArray (Array);
+
+NumberOfEvenNumbers (Array);
+
