b8a6bbd9c19ab50b0f432010a3ce8d7d5c9375e3
Zhanar S <zhan-ct@yandex.ru>
2023-01-16 18:10:07 +0300

    добавлено решение по Задаче 36 - сумма элементов по нечётным позициям

---
+++ DZ_02_Odd/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+void ShowArray(int[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write(Array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void SumOfNumbersOFOddPositions(int[] Array)
+{
+    int sum = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + Array[i];
+        }
+    }
+    Console.WriteLine($"Сумма элементов стоящих на нечётных позициях равна {sum}");
+}
+
+Console.Write("Введите количество элементов массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное значение массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] Array = CreateRandomArray(num, min, max + 1);
+ShowArray(Array);
+
+SumOfNumbersOFOddPositions(Array);
